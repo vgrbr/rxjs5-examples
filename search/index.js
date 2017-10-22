0ad5e1f4da39072012eeb8bf5c82804d5179b981
Vladimir Grubor <grubor@gmail.com>
2017-10-22 23:40:28 +0200

    style GH example

--- search/index.js
+++ search/index.js
@@ -1,10 +1,20 @@
 import Rx from 'rxjs/Rx'
+
 const $input = document.querySelector('input')
+const $result = document.querySelector('#results')
 const keyPresses$ = Rx.Observable.fromEvent($input, 'input')
-const displayItems = items => items.reduce((acc, item) =>
-  acc += `<li><img width="32" src="${item.avatar_url}" />${item.login}</li>`, '')
+
+const makeList = (acc, item) => acc += `
+  <li>
+    <img width="32" src="${item.avatar_url}" />
+    <a href="${item.html_url}">${item.login}</a>
+  </li>
+`
+
+const displayItems = items => items.reduce(makeList, '')
+
 const updateDom = result =>
-  document.querySelector('#results').innerHTML = `<ol>${displayItems(result.items)}</ol>`
+  $result.innerHTML = `<ol>${displayItems(result.items)}</ol>`
 
 const result$ = keyPresses$
   .debounce(() => Rx.Observable.timer(250))
@@ -12,7 +22,7 @@ const result$ = keyPresses$
   .map(key => {
     return Rx.Observable
       .ajax('https://api.github.com/search/users?q=' + key)
-      .retry(3)
+      .retry(2)
       .takeUntil(keyPresses$)
       .map(e => e.response)
   })
